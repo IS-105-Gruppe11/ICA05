b8d330689e29ca33edb2c434329b3852c11373c1
magnue16 <magnue16@student.uia.no>
2017-03-30 17:37:28 +0200

    la liga

--- server.go
+++ server.go
@@ -1,11 +1,55 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"net/http"
+	"log"
+	"encoding/json"
+	"io/ioutil"
+
+)
+
+
+type laLiga struct {
+	Name string `json:"name"`
+	Clubs []struct {
+		Key string `json:"key"`
+		Name string `json:"name"`
+		Code string `json:"code"`
+	} `json:"clubs"`
+}
 
 func main() {
 	m := martini.Classic()
-	m.Get("/", func()  string {
-		return "Hello world!"
+
+	m.Use(render.Renderer(render.Options{
+		IndentJSON: true,
+	}))
+	m.Get("/", func(r render.Render, x *http.Request) {
+		r.HTML(200, "index", getLiga())
 	})
+	m.RunOnAddr(":8001")
 	m.Run()
+}
+
+
+// Get clubs in liga
+func getLiga() *laLiga {
+	readApi, err := http.Get("https://raw.githubusercontent.com/openfootball/" +
+		"football.json/master/2016-17/es.1.clubs.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err := ioutil.ReadAll(readApi.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clubs := &laLiga{}
+	if err := json.Unmarshal(bytes, &clubs); err != nil {
+		log.Fatal(err)
+
+	}
+	return clubs
+
 }
